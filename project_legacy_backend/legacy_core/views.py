6103a3886315da4c3ee43c54a90b21d43d2c9043
Prohibidou <xmaxiverax@gmail.com>
2025-07-01 00:47:09 -0300

    view changes

--- project_legacy_backend/legacy_core/views.py
+++ project_legacy_backend/legacy_core/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import redirect
 from rest_framework.authtoken.models import Token
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+from allauth.socialaccount.helpers import complete_social_login
 
 def google_login_callback(request):
-    print(f"User logged in: {request.user.username}, email: {request.user.email}")
-    token, created = Token.objects.get_or_create(user=request.user)
-    return redirect(f'http://localhost:8000/auth/callback?token={token.key}')
+    adapter = GoogleOAuth2Adapter(request)
+    app = adapter.get_provider().get_app(request)
+    client = adapter.get_client(request, app)
+    access_token = client.get_access_token(request.GET['code'])
+    social_login = adapter.complete_login(request, app, access_token)
+    social_login.user.save()
+    complete_social_login(request, social_login)
+    token, created = Token.objects.get_or_create(user=social_login.user)
+    return redirect(f'http://localhost:3000/auth/callback?token={token.key}')
